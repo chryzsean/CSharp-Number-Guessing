dd644807c51fd523ca7af38dfe1a31eb43124db9
chryzsean <bryantforbes7@gmail.com>
2021-10-16 16:43:41 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace NumberGuessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playAgain = true;
+            int min = 1;
+            int max = 100;
+            int guess;
+            int number;
+            int guesses;
+            String response;
+
+            while (playAgain)
+            {
+                guess = 0;
+                guesses = 0;
+                response = "";
+                number = random.Next(1, 101);
+
+                while (guess != number)
+                {
+                    Console.WriteLine("Guess a number between " + min + " - " + max + " : ");
+
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Guess: " + guess);
+
+
+                    if (guess > number)
+                    {
+                        Console.WriteLine(guess + " is to high");
+                    }
+                    else if (guess < number)
+                    {
+                        Console.WriteLine(guess + " is to low");
+                    }
+                    guesses++;
+                }
+
+                Console.WriteLine("number: " + number);
+                Console.WriteLine("YOU WIN!");
+                Console.WriteLine("Guesses: " + guesses);
+
+                Console.WriteLine("Would you like to play again? (Y/N): ");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+
+                if (response == "Y")
+                {
+                    playAgain = true;
+
+                }
+                else
+                {
+                    playAgain = false;
+
+                }
+
+            }
+
+            Console.WriteLine("Thanks for playing :)");
+
+        }
+    }
+}
